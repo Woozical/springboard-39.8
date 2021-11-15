77de569516bad8707877319ce2310feefd372b80
Mark <mark@example.dev>
2021-11-14 16:48:14 -0800

    expanded test coverage

--- todo-app/src/NewTodoForm.test.js
+++ todo-app/src/NewTodoForm.test.js
@@ -10,3 +10,38 @@ test("should match snapshot", () => {
   const {asFragment} = render(<NewTodoForm />);
   expect(asFragment()).toMatchSnapshot();
 });
+
+test("updates on field change", () => {
+  const r = render(<NewTodoForm />);
+  const input = r.getByLabelText("New Task:");
+  expect(input.value).toEqual("");
+  fireEvent.change(input, {target: {value: "Walk the plants"}});
+  expect(input.value).toEqual("Walk the plants");
+  expect(r.getByDisplayValue("Walk the plants")).toBeInTheDocument();
+});
+
+test("does not execute addItem callback if input field is empty", () => {
+  let x = 0;
+  const foo = () => { x = 5 };
+  const r = render(<NewTodoForm addItem={foo} />);
+  fireEvent.click(r.getByText("Add"));
+  expect(x).toEqual(0);
+});
+
+test("executes addItem callback if input field has text", () => {
+  let x = 0;
+  const foo = () => { x = 5 };
+  const r = render(<NewTodoForm addItem={foo} />);
+  const input = r.getByLabelText("New Task:");
+  fireEvent.change(input, {target: {value: "Water the cats"}});
+  fireEvent.click(r.getByText("Add"));
+  expect(x).toEqual(5);
+});
+
+test("resets input field on form submission", () => {
+  const r = render(<NewTodoForm addItem={() => {}} />);
+  const input = r.getByLabelText("New Task:");
+  fireEvent.change(input, {target: {value: "Water the cats"}});
+  fireEvent.click(r.getByText("Add"));
+  expect(input.value).toEqual("");
+});
--- todo-app/src/Todo.test.js
+++ todo-app/src/Todo.test.js
@@ -10,3 +10,13 @@ test("matches snapshot", () => {
   const { asFragment } = render(<Todo task="Hello World" />);
   expect(asFragment()).toMatchSnapshot();
 });
+
+test("executes deleteItem callback on button click", () => {
+  let x = 0;
+  const foo = () => {x = 5};
+  const r = render(<Todo deleteItem={foo} task="Hello World" />);
+
+  expect(x).toEqual(0);
+  fireEvent.click(r.getByText("X"));
+  expect(x).toEqual(5);
+});
